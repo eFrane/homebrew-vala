dd1d7b98069e0ad79b640ed7c8b4f38528a88d3c
Stefan Graupner <stefan.graupner@gmail.com>
2016-11-09 17:05:10 +0100

    Updated liboparl

--- Formula/liboparl.rb
+++ Formula/liboparl.rb
@@ -1,11 +1,20 @@
-class LibOparl < Formula
-    desc ""
+class Liboparl < Formula
+    desc "A library to access OParl civic information endpoints"
     homepage "https://github.com/OParl/liboparl"
     url "https://github.com/OParl/liboparl.git"
+    version "master"
+
+    depends_on "glib"
+    depends_on "json-glib"
+
+    depends_on "meson" => :build
+    depends_on "vala" => :build
+    depends_on "ninja" => :build
 
     def install
-        system "./macos.build"
-        system "cd build && mesonbuild.py -Dprefix=#{prefix}"
+        system "mkdir build"
+        system "cd build && meson.py -Dbuild_valadoc=false .."
+        system "cd build && mesonconf.py -Dprefix=#{prefix}"
         system "cd build && ninja install"
     end
 end
